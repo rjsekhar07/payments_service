96f1f6712be7f58fec638ec61a06dac8aa5d1e2e
Sekhar Pidugu <sekhar.pidugu@Sekhar-Pidugu-RQK3X7YVTX.local>
2026-04-23 16:50:33 +0530

    updated transactions.py with app.database to app.dependencies in import

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from datetime import datetime
-from app.database import get_db
+from app.dependencies import get_db   
 from app.models.transaction import Transaction
 
 router = APIRouter()
