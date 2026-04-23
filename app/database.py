da1823ea1bfaf3553007abf364d7a98aaf9deb61
Sekhar Pidugu <sekhar.pidugu@Sekhar-Pidugu-RQK3X7YVTX.local>
2026-04-23 12:36:47 +0530

    fix database connection to use env variable

--- app/database.py
+++ app/database.py
@@ -1,17 +1,16 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from urllib.parse import quote_plus
 
-password = quote_plus("Rljs@1234567890")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"mysql+pymysql://root:{password}@localhost/payments_db"
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set")
 
 engine = create_engine(
     DATABASE_URL,
-    echo=True,
     pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(bind=engine)
-
 Base = declarative_base()
